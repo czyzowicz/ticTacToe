0a67085a1789698f3f7c1c088cb4565ccb7d3b7e
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-02-04 07:06:17 +0100

    add square and board component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss']
 })
-export class SquareComponent implements OnInit {
+export class SquareComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  @Input() value: 'x' | 'o';
+ 
 }
