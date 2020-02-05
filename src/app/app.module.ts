d985a247fc35c5ac265914fdf49e71dbd967aaff
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-02-05 11:01:32 +0100

    nb lib add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
