cb4f53b4b962cf028879f0dcad2eb585a35e1fc8
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-02-05 06:51:24 +0100

    before adding new lib

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  squares: any[];
+  squares: string[];
   xIsNext: boolean;
   winner: string;
 
